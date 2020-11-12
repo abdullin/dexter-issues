74ab6fa7a2a6a7415213af4e8b0aa6cd403490c7
Rinat Abdullin <rinat.abdullin@gmail.com>
2020-11-12 15:17:33 +0500

    import images

--- import.sh
+++ import.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-venv/bin/gh2md HaddingtonDynamics/Dexter --login abdullin --idempoten issues.md
+venv/bin/gh2md HaddingtonDynamics/Dexter --login abdullin --idempotent issues.md
+rg 'https.*user-images.githubusercontent.com.*(png|jpg|gif)' issues.md --only-matching | xargs wget --directory-prefix=images  --timestamping
